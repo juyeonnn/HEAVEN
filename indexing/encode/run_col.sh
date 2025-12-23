556d5309577762df04f4c9b859364f8bef4bb9b3
juyeonnn <culy1125@yonsei.ac.kr>
2025-12-23 05:05:07 +0000

    update encode

--- indexing/encode/run_col.sh
+++
@@ -1,7 +0,0 @@
-for encoder_type in colpali colqwen2 colqwen25; do
-    for dataset in M3DocVQA OpenDocVQA ViMDoc ViDoSeek; do
-        python encoder.py --encoder_type $encoder_type --dataset $dataset &
-    done
-    wait
-done
-
--- indexing/encode/run_dse.sh
+++
@@ -1,6 +0,0 @@
-for encoder_type in dse gme visret; do
-    for dataset in ViMDoc ViDoSeek M3DocVQA OpenDocVQA; do
-        python encoder.py --encoder_type $encoder_type --dataset $dataset &
-    done
-    wait
-done
